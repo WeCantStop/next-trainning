705e7daf95846d5ec4a823eeb7c16ef684902c36
panwei <panwei@zhongan.com>
2018-11-26 11:02:25 +0800

    add reset css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,8 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="viewport" content="width=device-width,initial-scale=1.0 user-scalable=no" />
-          <meta name="referrer" content="never" />
+          {/* <meta name="referrer" content="never" /> */}
+          <link rel="stylesheet" href="/static/css/reset.css"/>
         </Head>
         <body className="custom_class">
           <Main />
