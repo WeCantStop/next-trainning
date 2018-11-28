ffa7847b1d93ddfae02f488858157d929d6a3d56
panwei <panwei@zhongan.com>
2018-11-28 18:33:29 +0800

    add topic title

--- pages/topic/index.tsx
+++ pages/topic/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import Router from 'next/router'
+import moment from 'moment'
 import { get } from '../../tools/fetch'
 import './index.scss'
 import { genTypeText } from '@components/index/ListItem'
@@ -28,15 +29,26 @@ class Topic extends Component<any> {
     Router.back()
   }
 
+  formatData = (date) => {
+    console.log(date)
+    console.log(new Date(date).getTime())
+    return moment(new Date().getTime()).format('YYYY-MM-DD HH:mm:ss')
+  }
+
   render = () => {
     console.log(this.topic)
-    const { title, content } = this.topic
+    const { title, content, author, visit_count, create_at } = this.topic
     return (
       <div>
         <Header clickLeftBtn={this.goBack} />
         <div className="topic-title">
           <span className="topic-title-bandge">{genTypeText(this.topic)}</span>
           <span>{title}</span>
+        </div>
+        <div className="sub-title">
+          <span>{this.formatData(create_at)}</span>
+          <span>作者：{author.loginname}</span>
+          <span>浏览量：{visit_count}</span>
         </div>
         <div className="rich-container" dangerouslySetInnerHTML={{ __html: content }} />
         <Rocket />
